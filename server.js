a95202b7a072fb56761b519f642d6dfcf2a1d96c
andrelobo55 <loboandre55@gmail.com>
2025-08-09 14:39:37 -0300

    fix: adding dotenv module

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const agentesRoutes = require("./routes/agentesRoutes");
 const casosRoutes = require("./routes/casosRoutes");
 const setupSwagger = require('./docs/swagger');
+require('dotenv').config();
 
 app.use(express.json());
 app.use("/agentes", agentesRoutes);
