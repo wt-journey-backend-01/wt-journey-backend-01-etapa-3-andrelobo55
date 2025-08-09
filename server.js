5ea6c3350122a26269faff2a307165832b0d6c73
andrelobo55 <loboandre55@gmail.com>
2025-08-09 11:55:21 -0300

    feat: creating error middleware

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use("/agentes", agentesRoutes);
 app.use("/casos", casosRoutes);
 setupSwagger(app);
 
-app.use((req, res, next) => {
-    res.status(404).json({ message: "Rota não encontrada." });
+app.use((err, req, res, next) => {
+  if (err.name === 'API Error') {
+    return res.status(err.status).json({ message: err.message });
+  }
+  console.error(err);
+  res.status(500).json({ message: 'Erro interno do servidor' });
 });
 
 app.listen(PORT, () => {
