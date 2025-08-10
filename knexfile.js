a176ec055df7b79590fb06d07428a38eb3feea3c
andrelobo55 <loboandre55@gmail.com>
2025-08-10 03:16:23 -0300

    fix: changing knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,3 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-
 require('dotenv').config();
 
 module.exports = {
@@ -11,23 +5,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    },
-    migrations: {
-        directory: './db/migrations',
-      },
-    seeds: {
-        directory: './db/seeds',
-      },
-  },
-  ci: {
-    client: 'pg',
-    connection: {
-      host: 'postgres', // Using the service name as the host
+      host: process.env.DB_HOST || '127.0.0.1',
       port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
@@ -39,6 +17,23 @@ module.exports = {
     seeds: {
       directory: './db/seeds',
     },
-  }
+  },
+
+  ci: {
+    client: 'pg',
+    connection: {
+      host: 'postgres', // geralmente usado em ambiente Docker CI
+      port: 5432,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
 
 };
